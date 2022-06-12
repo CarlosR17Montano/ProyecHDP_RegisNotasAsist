e0129d43bc06b24c6c9c3482fda99b837041c941
fs17006 <fs17006@ues.edu.sv>
2022-06-12 16:48:31 -0500

    se hacen varios cambios ademas de agregacin de caracteristicas a las etq

--- Form/formRegistroDocenteMateria.php
+++ Form/formRegistroDocenteMateria.php
@@ -13,8 +13,9 @@ require("../Conex.php");
         <script type="text/javascript" src="../Js/vanadium.js"></script>
         <link rel="stylesheet" type="text/css" href="../Js/CSS/style.css" media="screen, tv, projection, print" />
         <link rel="stylesheet" type="text/css" href="../Js/CSS/formulario.css" media="screen, tv, projection, print" />
+        <link href="../Js/CSS/estilos.css" rel="stylesheet" type="text/css">
         <meta charset="UTF-8">
-        <title></title>
+        <title>Asignacion de docente a materia</title>
     </head>
     <body>
          <h2>FORMULARIO DE ASIGNACION DE DOCENTE A MATERIA</h2>
@@ -22,24 +23,24 @@ require("../Conex.php");
             <fieldset>
                 <label for="DatoIngreNotaEstu">Seleccione Registro del Docente</label><br>
                 <select id="personal" name="AsigDocenMateria" class=":required">
-                    <option value="" selected="selected">Seleccionar Codigo Docente</option>
+                    <option class="opcion" value="" selected="selected">Seleccionar Codigo Docente</option>
                     <?php
                     $sqlDatosDocente = "SELECT `NombresDocente`,`ApellidosDocente`,`CodigoDocente` FROM `docente`";
                     $resultDatoDocente = mysqli_query($con, $sqlDatosDocente) or die("database error:" . mysqli_error($con));
                     while ($DatoDocen = mysqli_fetch_assoc($resultDatoDocente)) {
                         ?>
-                        <option value="<?php echo $DatoDocen["CodigoDocente"]; ?>"><?php echo $DatoDocen["NombresDocente"] . " " . $DatoDocen["ApellidosDocente"] . "-" . $DatoDocen["CodigoDocente"]; ?></option>
+                        <option class="opcion" value="<?php echo $DatoDocen["CodigoDocente"]; ?>"><?php echo $DatoDocen["NombresDocente"] . " " . $DatoDocen["ApellidosDocente"] . "-" . $DatoDocen["CodigoDocente"]; ?></option>
                     <?php } ?>
                 </select><br>
                 <label for="DatoIngreNotaEstu">Seleccione Materia </label><br>
                 <select id="personal" name="IdAsignMateria" class=":required">
-                    <option value="" selected="selected">Seleccionar Materia</option>
+                    <option class="opcion" value="" selected="selected">Seleccionar Materia</option>
                     <?php
                     $sqlDatosMateria = "SELECT IdMateria,`NombreMateria`,`CodMateriaAnio` FROM `materia`";
                     $resultDatoMateria = mysqli_query($con, $sqlDatosMateria) or die("database error:" . mysqli_error($con));
                     while ($DatoMate = mysqli_fetch_assoc($resultDatoMateria)) {
                         ?>
-                        <option value="<?php echo $DatoMate["IdMateria"]; ?>"><?php echo $DatoMate["CodMateriaAnio"] . "-" . $DatoMate["NombreMateria"]; ?></option>
+                        <option class="opcion" value="<?php echo $DatoMate["IdMateria"]; ?>"><?php echo $DatoMate["CodMateriaAnio"] . "-" . $DatoMate["NombreMateria"]; ?></option>
                     <?php } ?>
                 </select>
             </fieldset>
--- Form/formRegistroMateriaEstudiantes.php
+++ Form/formRegistroMateriaEstudiantes.php
@@ -13,37 +13,40 @@ require("../Conex.php");
         <script type="text/javascript" src="../Js/vanadium.js"></script>
         <link rel="stylesheet" type="text/css" href="../Js/CSS/style.css" media="screen, tv, projection, print" />
         <link rel="stylesheet" type="text/css" href="../Js/CSS/formulario.css" media="screen, tv, projection, print" />
+        <link href="../Js/CSS/estilos.css" rel="stylesheet" type="text/css">
         <meta charset="UTF-8">
         <title></title>
     </head>
     <body>
          <h2>FORMULARIO DE REGISTRO DE MATERIA PARA ESTUDIANTES</h2>
-         <form name="formDocenteAgregar" action="../Procesos/ProcesoRegistrarMateriaEstudiante.php" method="POST">
-            <fieldset>
-                <label for="DatoIngreNotaEstu">Seleccione Registro del Estudiante</label><br>
-                <select id="personal" name="RegisMaterCodigoEstu" class=":required">
-                    <option value="" selected="selected">Seleccionar Codigo Estudiante</option>
-                    <?php
-                    $sqlDatosEstudiante = "SELECT `NombresEstudiante`,`ApellidosEstudiente`,`CodigoEstudiante` FROM `estudiante`";
-                    $resultDatoEstudiante = mysqli_query($con, $sqlDatosEstudiante) or die("database error:" . mysqli_error($con));
-                    while ($DatoEstu = mysqli_fetch_assoc($resultDatoEstudiante)) {
-                        ?>
-                        <option value="<?php echo $DatoEstu["CodigoEstudiante"]; ?>"><?php echo $DatoEstu["NombresEstudiante"] . " " . $DatoEstu["ApellidosEstudiente"] . "-" . $DatoEstu["CodigoEstudiante"]; ?></option>
-                    <?php } ?>
-                </select><br>
-                <label for="DatoIngreNotaEstu">Seleccione Materia </label><br>
-                <select id="personal" name="IdMaterRegis" class=":required">
-                    <option value="" selected="selected">Seleccionar Materia</option>
-                    <?php
-                    $sqlDatosMateria = "SELECT IdMateria,`NombreMateria`,`CodMateriaAnio` FROM `materia`";
-                    $resultDatoMateria = mysqli_query($con, $sqlDatosMateria) or die("database error:" . mysqli_error($con));
-                    while ($DatoMate = mysqli_fetch_assoc($resultDatoMateria)) {
-                        ?>
-                        <option value="<?php echo $DatoMate["IdMateria"]; ?>"><?php echo $DatoMate["CodMateriaAnio"] . "-" . $DatoMate["NombreMateria"]; ?></option>
-                    <?php } ?>
-                </select>
-            </fieldset>
-            <input type="submit" value="Enviar">
-        </form>
+        <div>
+          <form name="formDocenteAgregar" action="../Procesos/ProcesoRegistrarMateriaEstudiante.php" method="POST">
+             <fieldset>
+                 <label for="DatoIngreNotaEstu">Seleccione Registro del Estudiante</label><br>
+                 <select id="personal" name="RegisMaterCodigoEstu" class=":required">
+                     <option class="opcion" value="" selected="selected">Seleccionar Codigo Estudiante</option>
+                     <?php
+                     $sqlDatosEstudiante = "SELECT `NombresEstudiante`,`ApellidosEstudiente`,`CodigoEstudiante` FROM `estudiante`";
+                     $resultDatoEstudiante = mysqli_query($con, $sqlDatosEstudiante) or die("database error:" . mysqli_error($con));
+                     while ($DatoEstu = mysqli_fetch_assoc($resultDatoEstudiante)) {
+                         ?>
+                         <option class="opcion" value="<?php echo $DatoEstu["CodigoEstudiante"]; ?>"><?php echo $DatoEstu["NombresEstudiante"] . " " . $DatoEstu["ApellidosEstudiente"] . "-" . $DatoEstu["CodigoEstudiante"]; ?></option>
+                     <?php } ?>
+                 </select><br>
+                 <label for="DatoIngreNotaEstu">Seleccione Materia </label><br>
+                 <select id="personal" name="IdMaterRegis" class=":required">
+                     <option class="opcion" value="" selected="selected">Seleccionar Materia</option>
+                     <?php
+                     $sqlDatosMateria = "SELECT IdMateria,`NombreMateria`,`CodMateriaAnio` FROM `materia`";
+                     $resultDatoMateria = mysqli_query($con, $sqlDatosMateria) or die("database error:" . mysqli_error($con));
+                     while ($DatoMate = mysqli_fetch_assoc($resultDatoMateria)) {
+                         ?>
+                         <option class="opcion" value="<?php echo $DatoMate["IdMateria"]; ?>"><?php echo $DatoMate["CodMateriaAnio"] . "-" . $DatoMate["NombreMateria"]; ?></option>
+                     <?php } ?>
+                 </select>
+             </fieldset>
+             <input type="submit" value="Enviar">
+         </form>
+        </div>
     </body>
 </html>
