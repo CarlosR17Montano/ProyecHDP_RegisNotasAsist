0ff70b2297e3606a42a822bf900ff3ecd1d49354
fs17006 <fs17006@ues.edu.sv>
2022-06-12 16:09:45 -0500

    SEGUNDO

--- index.php
+++ index.php
@@ -7,14 +7,25 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <link rel="icon" href="./img/school-flag-solid.ico">
+        <link href="./Js/CSS/estilos.css" rel="stylesheet" type="text/css">
+        <title>MENU</title>
     </head>
     <body>
-        <a href="Form/formRegistroDocente.html">Registro Docente</a><br>
-        <a href="Form/formRegistroEstudiante.html">Registro Estudiante</a><br>
-        <a href="Form/formRegistroMateria.html">Registro Materia</a><br>
-        <a href="Form/formRegistroMateriaEstudiantes.php">Registro de Materias a Estudiante</a><br>
-        <a href="Form/formRegistroDocenteMateria.php">Registro de Asignacion de Docente a Materia</a><br>
-        
+
+        <h1>
+          MENU REGISTRO
+        </h1>
+
+        <div class="contenedorDeContenedor">
+          <div class="contenedor">
+            <a href="Form/formRegistroDocente.html">Registro Docente</a><br>
+            <a href="Form/formRegistroEstudiante.html">Registro Estudiante</a><br>
+            <a href="Form/formRegistroMateria.html">Registro Materia</a><br>
+            <a href="Form/formRegistroMateriaEstudiantes.php">Registro de Materias a Estudiante</a><br>
+            <a href="Form/formRegistroDocenteMateria.php">Registro de Asignacion de Docente a Materia</a><br>
+
+          </div>
+        </div>
     </body>
 </html>
