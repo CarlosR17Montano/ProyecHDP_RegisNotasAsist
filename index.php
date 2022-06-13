786dbb47d6740a74f47a4287c543d089d1c16129
fs17006 <fs17006@ues.edu.sv>
2022-06-12 23:53:12 -0500

    ESTILOS APLICADOS EN EL INDEX

--- index.php
+++ index.php
@@ -7,14 +7,20 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <link href="./Js/CSS/estilos.css" rel="stylesheet" type="text/css">
+        <title>MENU</title>
     </head>
     <body>
-        <a href="Form/formRegistroDocente.html">Registro Docente</a><br>
-        <a href="Form/formRegistroEstudiante.html">Registro Estudiante</a><br>
-        <a href="Form/formRegistroMateria.html">Registro Materia</a><br>
-        <a href="Form/formRegistroMateriaEstudiantes.php">Registro de Materias a Estudiante</a><br>
-        <a href="Form/formRegistroDocenteMateria.php">Registro de Asignacion de Docente a Materia</a><br>
-        
+      <h1> MENU REGISRO </h1>
+      <div class="contenedorDeContenedor">
+        <div class="contenedor">
+          <a href="Form/formRegistroDocente.html">Registro Docente</a><br>
+          <a href="Form/formRegistroEstudiante.html">Registro Estudiante</a><br>
+          <a href="Form/formRegistroMateria.html">Registro Materia</a><br>
+          <a href="Form/formRegistroMateriaEstudiantes.php">Registro de Materias a Estudiante</a><br>
+          <a href="Form/formRegistroDocenteMateria.php">Registro de Asignacion de Docente a Materia</a><br>
+
+        </div>
+      </div>
     </body>
 </html>
